3956d620dd0729f9ab33ad43bec68d6811de569e
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-11 15:20:37 +0200

    deleted SwaggerHandler class

---
+++ src/main/java/com/acoustic/apigateway/swagger/SwaggerProvider.java
@@ -0,0 +1,41 @@
+package com.acoustic.apigateway.swagger;
+
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class SwaggerProvider implements SwaggerResourcesProvider {
+    public static final String API_URI = "/v3/api-docs";
+
+    private final RouteDefinitionLocator routeLocator;
+
+    public SwaggerProvider(RouteDefinitionLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        routeLocator.getRouteDefinitions().subscribe(routeDefinition -> {
+            String resourceName = routeDefinition.getId();
+            String location = routeDefinition.getPredicates().get(0).getArgs().get("_genkey_0").replace("/**", API_URI);
+            resources.add(swaggerResource(resourceName, location));
+        });
+
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("3.0");
+        return swaggerResource;
+    }
+}
